bd98bdea722df4dfd43333cfab62fe05a4eb6f36
Bipul Roy <bbipulroy@users.noreply.github.com>
2017-07-17 17:40:57 +0700

    At first upload project base

---
+++ application/config/table_system.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['table_system_user_group'] = 'arm_tms.tms_system_user_group';
+$config['table_system_task'] = 'arm_tms.tms_system_task';
+$config['table_system_user_group_role'] = 'arm_tms.tms_system_user_group_role';
+$config['table_system_assigned_group'] = 'arm_tms.tms_system_assigned_group';
+$config['table_system_history'] = 'arm_tms.tms_system_history';
+$config['table_system_session'] = 'arm_tms.tms_system_session';
+$config['table_system_site_offline'] = 'arm_tms.tms_system_site_offline';
+$config['table_system_history_hack'] = 'arm_tms.tms_system_history_hack';
