5a8d297fdc09936883e248706915489ca49c89dd
saifultitu90 <saifultitu90@gmail.com>
2017-07-22 08:13:14 +0600

    table_tms updated

--- application/config/table_tms.php
+++ application/config/table_tms.php
@@ -2,3 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['tms_sample']='';
+// Office Staff
+$config['table_tms_setup_subordinate_employee'] = 'arm_tms.tms_setup_subordinate_employee';
+$config['table_tms_setup_coworker'] = 'arm_tms.tms_setup_coworker';
+
