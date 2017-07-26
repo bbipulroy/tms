c513ca83a8144eea26e10b750e3fcf6150fa0a75
Bipul Roy <bbipulroy@users.noreply.github.com>
2017-07-26 16:54:47 +0700

    table_tms.php file modify

--- application/config/table_tms.php
+++ application/config/table_tms.php
@@ -5,8 +5,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['table_tms_setup_subordinate_employee'] = 'arm_tms_2017_07.tms_setup_subordinate_employee';
 $config['table_tms_setup_coworker'] = 'arm_tms_2017_07.tms_setup_coworker';
 $config['table_tms_setup_assign_departments'] = 'arm_tms_2017_07.tms_setup_assign_departments';
-$config['table_tms_activities_assign_user_regular_task'] = 'arm_tms_2017_07.tms_activities_assign_user_regular_task';
+
 //interval setup
 $config['table_tms_setup_interval'] = 'arm_tms_2017_07.tms_setup_interval';
+
 // regular task
 $config['table_tms_activities_regular_task'] = 'arm_tms_2017_07.tms_activities_regular_task';
+$config['table_tms_activities_assign_user_regular_task'] = 'arm_tms_2017_07.tms_activities_assign_user_regular_task';
