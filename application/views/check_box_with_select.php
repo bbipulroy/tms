4693628a1a7faa8723dd45793b2fd6c44bf712ee
saifultitu90 <saifultitu90@gmail.com>
2017-08-07 15:07:59 +0600

    check_box_with_select view updated

--- application/views/check_box_with_select.php
+++ application/views/check_box_with_select.php
@@ -4,7 +4,7 @@ foreach($items as $item)
     ?>
     <div class="checkbox">
         <label title="<?php echo $item['name']; ?>">
-            <input type="checkbox" name="users[]" value="<?php echo $item['id']; ?>"><?php echo $item['name'].' - '.$item['employee_id'].' ('.$item['designation_name'].')'; ?>
+            <input type="checkbox" name="users[]" value="<?php echo $item['id']; ?>"<?php if(in_array($item['id'],$assigned_users)){echo ' checked';} ?>><?php echo $item['name'].' - '.$item['employee_id'].' ('.$item['designation_name'].')'; ?>
         </label>
     </div>
 <?php
