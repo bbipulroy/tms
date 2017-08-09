42c2d353e4d3e9cdcd0b7bbfcee1f20e759c4c18
Bipul Roy <bbipulroy@users.noreply.github.com>
2017-08-09 15:11:44 +0700

    remove ordering from details.php view

--- application/views/regular_tasks/details.php
+++ application/views/regular_tasks/details.php
@@ -97,14 +97,6 @@ $CI->load->view('action_buttons',array('action_buttons'=>$action_buttons));
             ?>
         </div>
     </div>
-    <div class="row show-grid">
-        <div class="col-xs-4">
-            <label class="control-label pull-right"><?php echo $this->lang->line('LABEL_ORDER');?></label>
-        </div>
-        <div class="col-sm-4 col-xs-8">
-            <?php echo $item['ordering'];?>
-        </div>
-    </div>
 </div>
 
 <div class="clearfix"></div>
